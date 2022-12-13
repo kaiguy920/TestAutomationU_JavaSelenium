d09f5b2fe0c492976e1b84ad930aa186948a00a4
CapcoKai <kai.smith@capco.com>
2022-12-13 11:05:26 -0500

    Adds base testing script

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,5 +1,6 @@
 package base;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +12,13 @@ public class BaseTests {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
+
+        int width = 375;
+        int height = 812;
+        driver.manage().window().setSize(new Dimension(width, height));
+
         System.out.println(driver.getTitle());
+        driver.quit();
     }
 
     public static void main(String args[]){
