34fde28c43bbd02a2db75704d115cc38c750c018
CapcoKai <kai.smith@capco.com>
2022-12-14 15:26:37 -0500

    Adds challenge script

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -13,11 +17,32 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        int width = 375;
-        int height = 812;
-        driver.manage().window().setSize(new Dimension(width, height));
+//         ** MANAGE WEB-PAGE **
+//        int width = 375;
+//        int height = 812;
+//        driver.manage().window().setSize(new Dimension(width, height));
 
-        System.out.println(driver.getTitle());
+//        ** FIND ONE ELEMENT **
+//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+//        inputsLink.click();
+
+//        ** FIND MULTIPLE ELEMENTS **
+//        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        System.out.println(links.size());
+//
+//         ** PRINT ELEMENT OFF PAGE **
+//        System.out.println(driver.getTitle());
+//
+//        ====================== TASK =================================
+//        1. click on "Shifting Content"
+        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
+        shiftingContentLink.click();
+//        2. click on the first example
+        WebElement exampleOneLink = driver.findElement(By.linkText("Example 1: Menu Element"));
+        exampleOneLink.click();
+//        3. print how many menu elements appear
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+        System.out.println(links.size());
         driver.quit();
     }
 
